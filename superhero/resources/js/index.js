8d656b91a8f9687fd617ceb7fbba4fb63726d430
Noa Lauvray <noa.lauvray@ynov.com>
2024-01-20 23:01:49 +0100

    modif_index_js

--- superhero/resources/js/index.js
+++ superhero/resources/js/index.js
@@ -1,55 +1,61 @@
+import React from 'react';
 import Heros from '.../superheroFront/ask-hero/src/app/Heros/page.tsx';
 import axios from './axios';
 import Head from 'next/head';
 import Link from 'next/link';
 
-export const Home = ({superheroes}) => {
-    return (
-        <>
-            <Head>
-                <title>Superhero - Home</title>
-            </Head>
+const Data = ({ superheroes }) => {
+  return (
+    <div>
+      <Head>
+        <title>Superhero</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <div className="flex flex-col gap-2">
-
-            {superheroes.map(superhero => (
-
-                <div className="flex flex-col gap-2">
-                    <h1 className="text-2xl font-bold">
-                        {superhero.name}
-                    </h1>
-                    <p>
-                        {superhero.superheroes.body}
-                    </p>
-
-                    <Link href="">
-                        <a className="text-blue-500 hover:underline">
-                            Read more 
-                        </a>
-                    </Link>
-                    
-                </div>
-            ))}
-            </div>
-        </>
-    )
-}
+      <main>
+        <h1>Superhero</h1>
+        <ul>
+          {superheroes.map(superhero => (
+            <li key={superhero.id}>
+              <Link href={`/superhero/${superhero.id}`}>
+                <a>{superhero.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
 
 export async function getServerSideProps() {
-    const response = await axios.get('../routes/api.php');
+  try {
+    const response = await axios.get('../public/index.php');
     const superheroes = response.data.data;
 
     return {
-        props: {
-            superheroes
-        }
-    }
+      props: {
+        superheroes,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        superheroes: [],
+      },
+    };
+  }
 }
 
-Home.getLayout = function getLayout(page) {
-    return (
-        <Heros>
-            {page}
-        </Heros>
-    )
-}
+Data.getLayout = function getLayout(page) {
+  return (
+    <Heros>
+      {page}
+    </Heros>
+  );
+};
+
+console.log(Data);
+
+export default Data;
